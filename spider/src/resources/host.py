c18c21b6ae72dc24d918e94b27865bf7c811e602
minibear <lei.zhang.bupt08@gmail.com>
2016-06-01 14:21:36 +0800

    ADD: Resources Module

---
+++ spider/src/resources/host.py
@@ -0,0 +1,13 @@
+# encoding=UTF-8
+# 资源管理组件, 主要用来管理网站, 网站url等相关配置
+
+class Resources:
+    def __init__(self, host, APIs):
+        """
+        资源类管理初始化
+        :param host: 爬虫网站HOST
+        :param APIs: API的列表, dict类型, 包含对应的url和parameters
+        """
+        super.__init__()
+        self.host = host
+        self.APIs = APIs
