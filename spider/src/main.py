cedda29784be8c5a176cf698e885029a14f86f61
minibear <lei.zhang.bupt08@gmail.com>
2016-06-01 21:24:58 +0800

    MOD:

--- spider/src/main.py
+++ spider/src/main.py
@@ -1,1 +1,7 @@
 # encoding=UTF-8
+from resources.fittime import config
+from spider.spider import FitTimeSpider
+
+
+if __name__ == '__main__':
+    spider = FitTimeSpider(config['fittime'])
--- spider/src/spider/spider.py
+++ spider/src/spider/spider.py
@@ -3,18 +3,36 @@ import requests as req
 from multiprocessing.dummy import Pool as ThreadPool
 
 
-class Spider:
-    def __init__(self, resource):
+class FitTimeSpider:
+    REQUEST_LIMIT = 200
+
+    def __init__(self, config):
         """
         初始化相关参数, 包括线程池, 资源类等信息
         :param resource: 资源
         """
-        super(Spider, self).__init__()
-        self.resource = resource
-        self.pool = ThreadPool(resource.pool_limit)
+        self.config = config
+        self.pool = ThreadPool(config.POOL_LIMIT)
 
-    def set_up(self):
+    def __set_up_(self):
         """
-        根据
+        初始化相关的逻辑
+        """
+        pass
+
+    def __form_url_(self, path_type, params):
+        """
+        生成对应的url
+        :param path_type: API的path
+        :param params: 对应的参数
+        :return: url字符串
+        """
+
+
+    def start(self, index):
+        """
+        开始抓取
+        :param index: 开始抓取的序列id
         :return:
         """
+        pass
